a83c7bcb07ce3be059da7284d6d0e6225f699dbf
jackkitte <jackkitte735@gmail.com>
2021-02-28 18:38:24 +0900

    POSTリクエストのハンドリング

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let todos = [
   {id: 2, title: '下書き', completed: true}
 ]
 const app = express()
+app.use(express.json())
 
 app.get('/api/todos', (req, res) => {
   if (!req.query.completed) {
@@ -13,6 +14,25 @@ app.get('/api/todos', (req, res) => {
   }
   const completed = req.query.completed === 'true'
   res.json(todos.filter(todo => todo.completed === completed))
+})
+
+let id = 2
+
+app.post('/api/todos', (req, res, next) => {
+  const {title} = req.body
+  if (typeof title !== 'string' || !title) {
+    const err = new Error('title is required')
+    err.statusCode = 400
+    return next(err)
+  }
+  const todo = {id: id += 1, title, completed: false}
+  todos.push(todo)
+  res.status(201).json(todo)
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.statusCode || 500).json({error: err.message})
 })
 
 app.listen(3000)
